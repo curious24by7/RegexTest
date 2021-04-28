c4dec94a0c8fe45e3e3441e97499d090063ad79a
sohamdhamnaskar <sohamdham123@gmail.com>
2021-04-28 22:58:32 +0530

    [soham]Added: Refactored Code completely implemented for Regex.

--- RegexTest/Program.cs
+++ RegexTest/Program.cs
@@ -20,13 +20,34 @@ namespace RegexTest
             switch (input)
             {
                 case 1:
-                    Console.WriteLine("Enter your First Name.");
-
+                    Console.WriteLine("Enter your First Name:");
                     RegexPatterns name = new RegexPatterns();
-
-                    Console.WriteLine("Enter First Name :");
                     bool result = name.ValidateName(Console.ReadLine());
                     Console.WriteLine(result);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter your Last Name:");
+                    RegexPatterns lastname = new RegexPatterns();
+                    bool resultlastname = lastname.ValidateLastName(Console.ReadLine());
+                    Console.WriteLine(resultlastname);
+                    break;
+                case 3:
+                    Console.WriteLine("Enter your Phone Number:");
+                    RegexPatterns phonenum = new RegexPatterns();
+                    bool resultphonenum = phonenum.ValidatePhoneNum(Console.ReadLine());
+                    Console.WriteLine(resultphonenum);
+                    break;
+                case 4:
+                    Console.WriteLine("Enter your Email Id:");
+                    RegexPatterns emailid = new RegexPatterns();
+                    bool resultemailid = emailid.ValidateEmail(Console.ReadLine());
+                    Console.WriteLine(resultemailid);
+                    break;
+                case 5:
+                    Console.WriteLine("Enter your Password:");
+                    RegexPatterns pass = new RegexPatterns();
+                    bool resultpass = pass.ValidatePassword(Console.ReadLine());
+                    Console.WriteLine(resultpass);
                     break;
             }
             Console.ReadKey();
--- RegexTest/RegexPatterns.cs
+++ RegexTest/RegexPatterns.cs
@@ -10,10 +10,30 @@ namespace RegexTest
     public class RegexPatterns
     {
         public string RegexName = "^[A-Z]{1}[a-z]{3,}$";
+        public string RegexLastName = "^[A-Z]{1}[a-z]{3,}$";
+        public string RegexPhoneNum = "^[9]{1}[1]{1}[ ]{1}[0-9]{10}$";
+        public string RegexEmail = "^[a-z0-9A-Z]+([.][a-zA-Z0-9]+)?[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,2})?$";
+        public string RegexPassword = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+]).{8,}$";
 
         public bool ValidateName(string firstname)
         {
             return Regex.IsMatch(firstname, RegexName);
+        }
+        public bool ValidateLastName(string lastname)
+        {
+            return Regex.IsMatch(lastname, RegexLastName);
+        }
+        public bool ValidatePhoneNum(string phonenum)
+        {
+            return Regex.IsMatch(phonenum, RegexPhoneNum);
+        }
+        public bool ValidateEmail(string email)
+        {
+            return Regex.IsMatch(email, RegexEmail);
+        }
+        public bool ValidatePassword(string password)
+        {
+            return Regex.IsMatch(password, RegexPassword);
         }
     }
 }
