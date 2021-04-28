84dbfac393bdde06a4006d40b81a84bb9c62f005
sohamdhamnaskar <sohamdham123@gmail.com>
2021-04-28 22:30:28 +0530

    [soham]Added: User registration Regex problem.

---
+++ RegexTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose what to Enter:");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("1. First Name.\n2. Last Name.\n3. Phone Number.\n4. Email Id.\n5. Password.");
+            Console.WriteLine("-------------------------");
+
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("Enter your First Name.");
+
+                    RegexPatterns name = new RegexPatterns();
+
+                    Console.WriteLine("Enter First Name :");
+                    bool result = name.ValidateName(Console.ReadLine());
+                    Console.WriteLine(result);
+                    break;
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ RegexTest/RegexPatterns.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexTest
+{
+    public class RegexPatterns
+    {
+        public string RegexName = "^[A-Z]{1}[a-z]{3,}$";
+
+        public bool ValidateName(string firstname)
+        {
+            return Regex.IsMatch(firstname, RegexName);
+        }
+    }
+}
