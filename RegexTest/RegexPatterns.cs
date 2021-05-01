602fc05930b37f44a6b3a07f956d4732b843e68d
sohamdhamnaskar <sohamdham123@gmail.com>
2021-05-01 12:18:40 +0530

    [soham]Changed:Naming Convention for RegexPatterns.

--- RegexTest/RegexPatterns.cs
+++ RegexTest/RegexPatterns.cs
@@ -9,31 +9,31 @@ namespace RegexTest
 {
     public class RegexPatterns
     {
-        public string RegexName = "^[A-Z]{1}[a-z]{3,}$";
-        public string RegexLastName = "^[A-Z]{1}[a-z]{3,}$";
-        public string RegexPhoneNum = "^[9]{1}[1]{1}[ ]{1}[0-9]{10}$";
-        public string RegexEmail = "^[a-z0-9A-Z]+([.][a-zA-Z0-9]+)?[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,2})?$";
-        public string RegexPassword = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+]).{8,}$";
+        public string FIRSTNAME_REGEX = "^[A-Z]{1}[a-z]{3,}$";
+        public string LASTNAME_REGEX = "^[A-Z]{1}[a-z]{3,}$";
+        public string PHONENUM_REGEX = "^[9]{1}[1]{1}[ ]{1}[0-9]{10}$";
+        public string EMAIL_REGEX = "^[a-z0-9A-Z]+([.][a-zA-Z0-9]+)?[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,2})?$";
+        public string PASSWORD_REGEX = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+]).{8,}$";
 
         public bool ValidateName(string firstname)
         {
-            return Regex.IsMatch(firstname, RegexName);
+            return Regex.IsMatch(firstname, FIRSTNAME_REGEX);
         }
         public bool ValidateLastName(string lastname)
         {
-            return Regex.IsMatch(lastname, RegexLastName);
+            return Regex.IsMatch(lastname, LASTNAME_REGEX);
         }
         public bool ValidatePhoneNum(string phonenum)
         {
-            return Regex.IsMatch(phonenum, RegexPhoneNum);
+            return Regex.IsMatch(phonenum, PHONENUM_REGEX);
         }
         public bool ValidateEmail(string email)
         {
-            return Regex.IsMatch(email, RegexEmail);
+            return Regex.IsMatch(email, EMAIL_REGEX);
         }
         public bool ValidatePassword(string password)
         {
-            return Regex.IsMatch(password, RegexPassword);
+            return Regex.IsMatch(password, PASSWORD_REGEX);
         }
     }
 }
