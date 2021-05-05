41c316943cb16bdb32e7bb5961f841465acb1332
sohamdhamnaskar <sohamdham123@gmail.com>
2021-05-05 10:22:30 +0530

    [soham]Added: MStest for Lambda expression on User registration

--- RegexTest/Program.cs
+++ RegexTest/Program.cs
@@ -6,51 +6,139 @@ using System.Threading.Tasks;
 
 namespace RegexTest
 {
-    class Program
+    class RegexProgram : Exception
     {
-        static void Main(string[] args)
+        public RegexProgram(String message) : base(message)
         {
+
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("-------------------------");
             Console.WriteLine("Choose what to Enter:");
             Console.WriteLine("-------------------------");
-            Console.WriteLine("1. First Name.\n2. Last Name.\n3. Phone Number.\n4. Email Id.\n5. Password.");
+            Console.WriteLine("1. First Name.\n2. Last Name.\n3. Phone Number.\n4. Email Id.\n5. Password.\n6. Exit");
             Console.WriteLine("-------------------------");
-
-            int input = Convert.ToInt32(Console.ReadLine());
-
-            switch (input)
+            var userinput = Console.ReadLine();
+            while (true)
             {
-                case 1:
-                    Console.WriteLine("Enter your First Name:");
-                    RegexPatterns name = new RegexPatterns();
-                    bool result = name.ValidateName(Console.ReadLine());
-                    Console.WriteLine(result);
-                    break;
-                case 2:
-                    Console.WriteLine("Enter your Last Name:");
-                    RegexPatterns lastname = new RegexPatterns();
-                    bool resultlastname = lastname.ValidateLastName(Console.ReadLine());
-                    Console.WriteLine(resultlastname);
-                    break;
-                case 3:
-                    Console.WriteLine("Enter your Phone Number:");
-                    RegexPatterns phonenum = new RegexPatterns();
-                    bool resultphonenum = phonenum.ValidatePhoneNum(Console.ReadLine());
-                    Console.WriteLine(resultphonenum);
-                    break;
-                case 4:
-                    Console.WriteLine("Enter your Email Id:");
-                    RegexPatterns emailid = new RegexPatterns();
-                    bool resultemailid = emailid.ValidateEmail(Console.ReadLine());
-                    Console.WriteLine(resultemailid);
-                    break;
-                case 5:
-                    Console.WriteLine("Enter your Password:");
-                    RegexPatterns pass = new RegexPatterns();
-                    bool resultpass = pass.ValidatePassword(Console.ReadLine());
-                    Console.WriteLine(resultpass);
-                    break;
+                try
+                {
+                    switch (userinput)
+                    {
+                        case "1":
+                            try
+                            {
+                                Console.WriteLine("Enter your First Name:");
+                                RegexPatterns name = new RegexPatterns();
+                                bool result = name.ValidateName(Console.ReadLine(), RegexPatterns.FIRSTNAME_REGEX);
+                                Console.WriteLine(result);
+                                if (result == false)
+                                {
+                                    throw new RegexProgram("Must contain characters only. Having first letter capital with minimum 3 characters.\n====================");
+                                }
+                            }
+                            catch (RegexProgram e)
+                            {
+                                Console.WriteLine("====================\nInvalid Input\n====================");
+                                Console.WriteLine(e.Message);
+                            }
+                            break;
+                        case "2":
+                            try
+                            {
+                                Console.WriteLine("Enter your Last Name:");
+                                RegexPatterns lastname = new RegexPatterns();
+                                bool resultlastname = lastname.ValidateLastName(Console.ReadLine(), RegexPatterns.LASTNAME_REGEX);
+                                Console.WriteLine(resultlastname);
+                                if (resultlastname == false)
+                                {
+                                    throw new RegexProgram("Must contain characters only. Having first letter capital with minimum 3 characters.\n====================");
+                                }
+                            }
+                            catch (RegexProgram e)
+                            {
+                                Console.WriteLine("====================\nInvalid Input\n====================");
+                                Console.WriteLine(e.Message);
+                            }
+                            break;
+
+                        case "3":
+                            try
+                            {
+                                Console.WriteLine("Enter your Phone Number:");
+                                RegexPatterns phonenum = new RegexPatterns();
+                                bool resultphonenum = phonenum.ValidatePhoneNum(Console.ReadLine(), RegexPatterns.PHONENUM_REGEX);
+                                Console.WriteLine(resultphonenum);
+                                if (resultphonenum == false)
+                                {
+                                    throw new RegexProgram("Must contain integers only. Starting with '91' and space and having exact 10 digits as Phone Number.\n====================");
+                                }
+                            }
+                            catch (RegexProgram e)
+                            {
+                                Console.WriteLine("====================\nInvalid Input\n====================");
+                                Console.WriteLine(e.Message);
+                            }
+                            break;
+
+                        case "4":
+                            try
+                            {
+                                Console.WriteLine("Enter your Email Id:");
+                                RegexPatterns emailid = new RegexPatterns();
+                                bool resultemailid = emailid.ValidateEmail(Console.ReadLine(), RegexPatterns.EMAIL_REGEX);
+                                Console.WriteLine(resultemailid);
+                                if (resultemailid == false)
+                                {
+                                    throw new RegexProgram("Must contain Alphabet and Numeric Combination only.\nEnding with proper domain address e.g. '@gmail.com'\n====================");
+                                }
+                            }
+                            catch (RegexProgram e)
+                            {
+                                Console.WriteLine("====================\nInvalid Input\n====================");
+                                Console.WriteLine(e.Message);
+                            }
+                            break;
+                        case "5":
+                            try
+                            {
+                                Console.WriteLine("Enter your Password:");
+                                RegexPatterns pass = new RegexPatterns();
+                                bool resultpass = pass.ValidatePassword(Console.ReadLine(), RegexPatterns.PASSWORD_REGEX);
+                                Console.WriteLine(resultpass);
+                                if (resultpass == false)
+                                {
+                                    throw new RegexProgram("Must contain Alphanumeric combination.\nConsisting atleast 1 uppercase character, atleast 1 numeric value.\nAnd exactly one special character.\n====================");
+                                }
+                            }
+                            catch (RegexProgram e)
+                            {
+                                Console.WriteLine("====================\nInvalid Input\n====================");
+                                Console.WriteLine(e.Message);
+                            }
+                            break;
+                        case "6":
+                            return;
+                        default:
+                            Console.WriteLine("!! Invalid Input !!");
+                            break;
+                    }
+                    Console.WriteLine("-------------------------");
+                    Console.WriteLine("Choose what to Enter:");
+                    Console.WriteLine("-------------------------");
+                    Console.WriteLine("1. First Name.\n2. Last Name.\n3. Phone Number.\n4. Email Id.\n5. Password.\n6. Exit");
+                    Console.WriteLine("-------------------------");
+                    userinput = Console.ReadLine();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
             }
-            Console.ReadKey();
+
         }
+
     }
 }
--- RegexTest/RegexPatterns.cs
+++ RegexTest/RegexPatterns.cs
@@ -9,31 +9,17 @@ namespace RegexTest
 {
     public class RegexPatterns
     {
-        public string RegexName = "^[A-Z]{1}[a-z]{3,}$";
-        public string RegexLastName = "^[A-Z]{1}[a-z]{3,}$";
-        public string RegexPhoneNum = "^[9]{1}[1]{1}[ ]{1}[0-9]{10}$";
-        public string RegexEmail = "^[a-z0-9A-Z]+([.][a-zA-Z0-9]+)?[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,2})?$";
-        public string RegexPassword = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+]).{8,}$";
+        public const string FIRSTNAME_REGEX = "^[A-Z]{1}[a-z]{3,}$";
+        public const string LASTNAME_REGEX = "^[A-Z]{1}[a-z]{3,}$";
+        public const string PHONENUM_REGEX = "^[9]{1}[1]{1}[ ]{1}[0-9]{10}$";
+        public const string EMAIL_REGEX = "^[a-z0-9A-Z]+([.][a-zA-Z0-9]+)?[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,2})?$";
+        public const string PASSWORD_REGEX = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+]).{8,}$";
 
-        public bool ValidateName(string firstname)
-        {
-            return Regex.IsMatch(firstname, RegexName);
-        }
-        public bool ValidateLastName(string lastname)
-        {
-            return Regex.IsMatch(lastname, RegexLastName);
-        }
-        public bool ValidatePhoneNum(string phonenum)
-        {
-            return Regex.IsMatch(phonenum, RegexPhoneNum);
-        }
-        public bool ValidateEmail(string email)
-        {
-            return Regex.IsMatch(email, RegexEmail);
-        }
-        public bool ValidatePassword(string password)
-        {
-            return Regex.IsMatch(password, RegexPassword);
-        }
+        //Func<string, string, bool> Test = (x, y) => x > y; ~~LAMBDA EXP
+        public Func<string, string, bool> ValidateName = (name, FIRSTNAME_REGEX) => Regex.IsMatch(name, FIRSTNAME_REGEX);
+        public Func<string, string, bool> ValidateLastName = (lastname, LASTNAME_REGEX) => Regex.IsMatch(lastname, LASTNAME_REGEX);
+        public Func<string, string, bool> ValidatePhoneNum = (phonenum, PHONENUM_REGEX) => Regex.IsMatch(phonenum, PHONENUM_REGEX);
+        public Func<string, string, bool> ValidateEmail = (email, EMAIL_REGEX) => Regex.IsMatch(email, EMAIL_REGEX);
+        public Func<string, string, bool> ValidatePassword = (password, PASSWORD_REGEX) => Regex.IsMatch(password, PASSWORD_REGEX);
     }
 }
