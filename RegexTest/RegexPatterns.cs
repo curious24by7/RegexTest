0c18d065ae2946e454e95c7518c1e807105efeed
sohamdhamnaskar <sohamdham123@gmail.com>
2021-05-05 08:22:04 +0530

    [soham]Made RegexPatterns Constant

--- RegexTest/RegexPatterns.cs
+++ RegexTest/RegexPatterns.cs
@@ -9,11 +9,11 @@ namespace RegexTest
 {
     public class RegexPatterns
     {
-        public string FIRSTNAME_REGEX = "^[A-Z]{1}[a-z]{3,}$";
-        public string LASTNAME_REGEX = "^[A-Z]{1}[a-z]{3,}$";
-        public string PHONENUM_REGEX = "^[9]{1}[1]{1}[ ]{1}[0-9]{10}$";
-        public string EMAIL_REGEX = "^[a-z0-9A-Z]+([.][a-zA-Z0-9]+)?[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,2})?$";
-        public string PASSWORD_REGEX = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+]).{8,}$";
+        public const string FIRSTNAME_REGEX = "^[A-Z]{1}[a-z]{3,}$";
+        public const string LASTNAME_REGEX = "^[A-Z]{1}[a-z]{3,}$";
+        public const string PHONENUM_REGEX = "^[9]{1}[1]{1}[ ]{1}[0-9]{10}$";
+        public const string EMAIL_REGEX = "^[a-z0-9A-Z]+([.][a-zA-Z0-9]+)?[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,2})?$";
+        public const string PASSWORD_REGEX = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+]).{8,}$";
 
         public bool ValidateName(string firstname)
         {
